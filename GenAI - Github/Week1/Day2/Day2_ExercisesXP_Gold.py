1887e0ab7213fa923230739bcf5ac2b5a222936b
maeva-mp <maeva.p.moonien@gmail.com>
2025-09-09 21:43:44 +0400

    ExercisesXP_Gold.py

--- GenAI - Github/Week1/Day2/Day2_ExercisesXP_Gold.py
+++ GenAI - Github/Week1/Day2/Day2_ExercisesXP_Gold.py
@@ -1,4 +1,6 @@
+#Exercise 1 : Hello World-I love Python
 print("Hello world\nHello world\nHello world\nHello world\nI love python\nI love python\nI love python\nI love python")
+#Exercise 2 : What is the Season ?
 month=int(input("Please enter a month number(1to12):"))
 if 3 <= month <= 5:
     print("It's Spring")
@@ -9,4 +11,5 @@ elif 9 <= month <= 11:
 elif month== 12 or 1 <= month <= 2:
     print("It's Winter")
 else:
+
     print('Invalid')
